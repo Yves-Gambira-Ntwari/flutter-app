a9e9bfad9a115ccae2d89aa1b7f05730e1a0c082
Gambira <gambirayves@gmail.com>
2026-01-15 15:52:17 +0200

    chore: using flutter and apply login form

--- android/lib/main.dart
+++ android/lib/main.dart
@@ -1,61 +1,103 @@
+import 'package:android/home_page.dart';
 import 'package:flutter/material.dart';
 
-
-class FormScreen extends StatefulWidget {
-  const FormScreen({super.key});
-
-  @override
-  State<FormScreen> createState() => _FormScreenState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _FormScreenState extends State<FormScreen> {
-  final _formKey = GlobalKey<FormState>();
+final _formKey = GlobalKey<FormState>();
 
-  String submittedName = '';
-  String submittedEmail = '';
+String name = '';
+String email = '';
+String password = '';
+
+@override
+void dispose() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Show Form Data')),
-      body: Center(
-        // padding: const EdgeInsets.all(16),
-        child: Column(
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
           children: [
-            Form(
-              key: _formKey,
+            Center(
               child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Name'),
-                    onSaved: (value) {
-                      submittedName = value!;
-                    },
+                  const Text(
+                    "Login here",
+                    style: TextStyle(fontSize: 30, color: Colors.blue),
                   ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Email'),
-                    onSaved: (value) {
-                      submittedEmail = value!;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      _formKey.currentState!.save();
-                      setState(() {});
-                    },
-                    child: const Text('Submit'),
+                  const SizedBox(height: 50),
+                  SizedBox(
+                    width: 300,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // Email input
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "Enter your Email",
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'This field is required';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 30),
+
+                          // Password input
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Enter your Password',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'This field is required';
+                              }
+
+                              return null;
+                            },
+                            onSaved: (value) {
+                              name = value!;
+                            },
+                          ),
+                          const SizedBox(height: 30),
+
+                          // Login button
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(double.infinity, 50),
+                              backgroundColor: Colors.blue,
+                            ),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                _formKey.currentState!.save();
+                                print(name);
+                              }
+                            },
+                            child: Text(
+                              "Login",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-
-            const SizedBox(height: 30),
-
-            Text('Name: $submittedName',
-                style: const TextStyle(fontSize: 18)),
-            Text('Email: $submittedEmail',
-                style: const TextStyle(fontSize: 18)),
           ],
         ),
       ),
