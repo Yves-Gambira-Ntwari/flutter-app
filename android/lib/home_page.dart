1a68d4f6e69080e640357006a1fb5fb39a13e4f7
Gambira <gambirayves@gmail.com>
2026-01-29 11:09:04 +0200

    chore: adding buttons of menu

--- android/lib/home_page.dart
+++ android/lib/home_page.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget{
+
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title:const Text("Home Page")),
-      body: const Center(
-        child: Text(
-          'Welcome to Home page',
-          style:TextStyle(fontSize: 20)
-        )
-      )
+      appBar: AppBar(title: const Text("Home Page")),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Text(
+                "Better",
+                style: TextStyle(
+                  color: Color.fromARGB(218, 209, 54, 54),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(218, 209, 54, 54),
+                ),
+                child: const Text(
+                  "Apply now",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.menu,
+                  color: const Color.fromARGB(218, 209, 54, 54),
+                  size: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
